7a1b063b69e4db12765e1621e6faf483eedffe81
Christine Palma <christine.palma@leafnode.io>
2021-01-18 10:39:04 +0800

    create sanity test suite

---
+++ Test Suites/Sanity Testing.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Sanity Testing</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b91462c1-784b-4e18-b80f-b394eb629be8</testSuiteGuid>
+   <testCaseLink>
+      <guid>2cbe9e15-9917-4695-bab4-3a6d54cc464b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
